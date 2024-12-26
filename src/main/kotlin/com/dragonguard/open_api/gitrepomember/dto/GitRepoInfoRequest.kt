35d3a0d02fbcf1fdbdeda13cc6e6bebd3fc6d300
김승진 <ohksj77@gmail.com>
2024-12-27 01:30:50 +0900

    hotfix: json field parse error

--- src/main/kotlin/com/dragonguard/open_api/gitrepomember/dto/GitRepoInfoRequest.kt
+++ src/main/kotlin/com/dragonguard/open_api/gitrepomember/dto/GitRepoInfoRequest.kt
@@ -1,8 +1,13 @@
 package com.dragonguard.open_api.gitrepomember.dto
 
+import com.fasterxml.jackson.annotation.JsonProperty
+
 data class GitRepoInfoRequest(
+    @JsonProperty("git_repo_id")
     val gitRepoId: Long?,
+    @JsonProperty("member_id")
     val memberId: Long?,
+    @JsonProperty("github_token")
     val githubToken: String?,
     val name: String?,
 )
